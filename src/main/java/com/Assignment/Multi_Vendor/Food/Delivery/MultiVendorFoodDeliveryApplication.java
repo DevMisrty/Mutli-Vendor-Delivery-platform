745436577a24464b25a53ad98381e50b9f02da97
dev Mistry <dev.mistry.agileinfoways@gmail.com>
2025-08-25 09:53:33 +0530

    initial commit

---
+++ src/main/java/com/Assignment/Multi_Vendor/Food/Delivery/MultiVendorFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.Assignment.Multi_Vendor.Food.Delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiVendorFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiVendorFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Assignment/Multi_Vendor/Food/Delivery/MultiVendorFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assignment.Multi_Vendor.Food.Delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiVendorFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
