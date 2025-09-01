895f60ed51a2879d7935263d4651b5cac0e0cc47
dev Mistry <dev.mistry.agileinfoways@gmail.com>
2025-09-01 10:15:36 +0530

    1

--- src/main/java/com/Assignment/Multi_Vendor/Food/Delivery/GlobalExceptionHandler/AdminControllerExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.Assignment.Multi_Vendor.Food.Delivery.GlobalExceptionHandler;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice   
-public class AdminControllerExceptionHandler {
-}
